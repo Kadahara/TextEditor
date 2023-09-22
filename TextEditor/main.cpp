0107d503ef9179de811b8549e547530e9c404420
User <User@WIN-E8F88FA2UKH>
2023-09-22 12:08:42 +0300

    Font

--- TextEditor/main.cpp
+++ TextEditor/main.cpp
@@ -3,13 +3,18 @@
 #include<Richedit.h>
 #include<cstdio>
 #include"resource.h"
+#include<CommCtrl.h>
 
 CONST CHAR g_sz_WINDOW_CLASS[] = "My Text Editor";
+HFONT g_hFont = (HFONT)GetStockObject(DEFAULT_GUI_FONT);
+COLORREF g_RGB_Text = RGB(0, 0, 0);
 
 LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
-BOOL LoadTextFileToEdit(HWND hEdit, LPSTR lpszFileName, LPSTR lpszFileText);
-BOOL SaveTextFileFromEdit(HWND hEdit, LPSTR lpszFileName, LPSTR lpszText);
+BOOL LoadTextFileToEdit(HWND hEdit, LPSTR lpszFileName);
+BOOL SaveTextFileFromEdit(HWND hEdit, LPSTR lpszFileName);
 BOOL FileWasChanger(HWND hEdit, LPSTR lpszText, LPSTR lpszFileText);
+
+VOID SelectFont(HWND hwnd);
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
 {
@@ -41,7 +46,7 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 	//2)Создаем окна:
 	HWND hwnd = CreateWindowEx
 	(
-		NULL,
+		WS_EX_ACCEPTFILES,
 		g_sz_WINDOW_CLASS,
 		g_sz_WINDOW_CLASS,
 		WS_OVERLAPPEDWINDOW,
@@ -86,30 +91,25 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		GetClientRect(hwnd, &rect);
 		HWND hEdit = CreateWindowEx
 		(
-			NULL, RICHEDIT_CLASS, " ",
-			WS_CHILD | WS_VISIBLE | WS_BORDER | ES_MULTILINE,
-			rect.left, rect.top ,
+			NULL, RICHEDIT_CLASS, "",
+			WS_CHILD | WS_VISIBLE | WS_BORDER | ES_MULTILINE | WS_VSCROLL,
+			rect.left, rect.top,
 			rect.right - rect.left, rect.bottom - rect.top,
 			hwnd,
 			(HMENU)IDC_EDIT,
 			GetModuleHandle(NULL),
 			0
 		);
-		CreateWindowEx
-		(
-			NULL, "STATUSCLASSNAME", g_sz_WINDOW_CLASS,
-			WS_CHILD | WS_VISIBLE | WS_BORDER,
-			CW_USEDEFAULT,
-			CW_USEDEFAULT,
-			CW_USEDEFAULT,
-			CW_USEDEFAULT,
+		HWND hStatus = CreateWindowEx
+		( 
+			NULL, STATUSCLASSNAME, NULL,
+			WS_CHILD | WS_VISIBLE,
+			0,0,0,0,
 			hwnd,
-			(HMENU)IDSTATUS,
+			(HMENU)ID_STATUS,
 			GetModuleHandle(NULL),
 			0
 		);
-
-
 	}
 	break;
 	case WM_MOVE: 
@@ -127,13 +127,82 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 
 
 	break;
+	case WM_SIZE:
+	{
+		HWND hStatus = GetDlgItem(hwnd, ID_STATUS);
+		RECT rcStatus;
+		SendMessage(hStatus, WM_SIZE, 0, 0);
+		GetWindowRect(hStatus, &rcStatus);
+		int iStatusHeight = rcStatus.bottom - rcStatus.top;
 
+		RECT rcClient;
+		HWND hEdit = GetDlgItem(hwnd, IDC_EDIT);
+		GetClientRect(hwnd, &rcClient);
+		int iEditHeight = rcClient.bottom - iStatusHeight;
+
+		SetWindowPos(hEdit, NULL, 0, 0, rcClient.right - rcClient.left, iEditHeight, SWP_NOZORDER);
+	}
+	break;
+	case WM_DROPFILES:
+	{
+		HWND hEdit = GetDlgItem(hwnd, IDC_EDIT);
+		DragQueryFile((HDROP)wParam, 0, szFileName, MAX_PATH);
+		LoadTextFileToEdit(hEdit, szFileName);
+		DWORD dwTextLength = SendMessage(hEdit, WM_GETTEXTLENGTH, 0, 0);
+		lpszFileText = (LPSTR)GlobalAlloc(GPTR, dwTextLength++);
+		SendMessage(hEdit, WM_GETTEXT, dwTextLength++, (LPARAM)lpszFileText);
+
+		HWND hStatus = GetDlgItem(hwnd, ID_STATUS);
+		SendMessage(hStatus, SB_SETTEXT, 0, (LPARAM)szFileName);
+		CHAR szTitle[FILENAME_MAX]{};
+		sprintf(szTitle, "%s - %s", g_sz_WINDOW_CLASS, strrchr(szFileName, '\\') + 1);
+		SendMessage(hwnd, WM_SETTEXT, 0, (LPARAM)szTitle);
+	}
+		break;
 	case WM_COMMAND:
 	{
 		switch (LOWORD(wParam))
 		{
+		case ID_FILE_NEW:
+		{
+			HWND hEdit = GetDlgItem(hwnd, IDC_EDIT);
+			DWORD dwTextLength = SendMessage(hEdit, WM_GETTEXTLENGTH, 0, 0);
+			LPSTR lpszText = (LPSTR)GlobalAlloc(GPTR, dwTextLength + 1);
+			SendMessage(hEdit, WM_GETTEXT, dwTextLength + 1, (LPARAM)lpszText);
+			if (lpszFileText && strcmp(lpszFileText, lpszText) || lpszFileText == 0 && lpszText[0])
+			{
+				switch (INT answer = MessageBox(hwnd, "Сохранить изменения в файле?", "Question", MB_YESNOCANCEL | MB_ICONQUESTION))
+				{
+				case IDYES: SendMessage(hwnd, WM_COMMAND, ID_FILE_SAVE, 0);
+				case IDNO:
+					lpszFileText = NULL;
+					szFileName[0] = 0;
+					SendMessage(hEdit, WM_SETTEXT, 0, (LPARAM)"");
+				case IDCANCEL: break;
+				}
+			}
+		}
+		break;
 		case ID_FILE_OPEN:
 		{
+			BOOL cansel = FALSE;
+			HWND hEdit = GetDlgItem(hwnd, IDC_EDIT);
+			DWORD dwTextLength = SendMessage(hEdit, WM_GETTEXTLENGTH, 0, 0);
+			LPSTR lpszText = (LPSTR)GlobalAlloc(GPTR, dwTextLength + 1);
+			SendMessage(hEdit, WM_GETTEXT, dwTextLength + 1, (LPARAM)lpszText);
+			if (lpszFileText && strcmp(lpszFileText, lpszText) || lpszFileText == 0 && lpszText[0])
+			{
+				switch (INT answer = MessageBox(hwnd, "Сохранить изменения в файле?", "Question", MB_YESNOCANCEL | MB_ICONQUESTION))
+				{
+				case IDYES: SendMessage(hwnd, WM_COMMAND, ID_FILE_SAVE, 0);
+				case IDCANCEL: cansel = TRUE; break;
+				/*case IDNO:
+					lpszFileText = NULL;
+					szFileName[0] = 0;
+					SendMessage(hEdit, WM_SETTEXT, 0, (LPARAM)"");*/
+				}
+				if (cansel)break;
+			}
 			//CHAR szFileName[MAX_PATH]{};
 
 			OPENFILENAME ofn;
@@ -150,21 +219,34 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			if (GetOpenFileName(&ofn))
 			{
 				HWND hEdit = GetDlgItem(hwnd, IDC_EDIT);
-				LoadTextFileToEdit(hEdit, szFileName, lpszFileText);
-			}
+				LoadTextFileToEdit(hEdit, szFileName);
 
-			CONST INT SIZE = 256;
-			CHAR sz_buffer[SIZE] = {};
-			sprintf(sz_buffer, szFileName);
-			SendMessage(hwnd, WM_SETTEXT, 0, (LPARAM)sz_buffer);
+				DWORD dwTextLength = SendMessage(hEdit, WM_GETTEXTLENGTH, 0, 0);
+				lpszFileText = (LPSTR)GlobalAlloc(GPTR, dwTextLength + 1);
+				SendMessage(hEdit, WM_GETTEXT, dwTextLength + 1, (LPARAM)lpszFileText);
+
+				CHAR szTitle[FILENAME_MAX]{};
+				sprintf(szTitle, "%s - %s", g_sz_WINDOW_CLASS, strrchr(szFileName, '\\')+1);
+				SendMessage(hwnd, WM_SETTEXT, 0, (LPARAM)szTitle);
+			}
+			//CONST INT SIZE = 256;
+			/*CHAR sz_buffer[FILENAME_MAX] = {};
+			sprintf(sz_buffer, szFileName, g_sz_WINDOW_CLASS);*/
+			HWND hStatusBar = GetDlgItem(hwnd, ID_STATUS);
+			SendMessage(hStatusBar, WM_SETTEXT, 0, (LPARAM)szFileName);
 
 		}break;
+
 		case ID_FILE_SAVE:
 		{
 			if (szFileName[0])
 			{
 				
-				SaveTextFileFromEdit(GetDlgItem(hwnd, IDC_EDIT), szFileName, lpszFileText);
+				SaveTextFileFromEdit(GetDlgItem(hwnd, IDC_EDIT), szFileName);
+				HWND hEdit = GetDlgItem(hwnd, IDC_EDIT);
+				DWORD dwTextLength = SendMessage(hEdit, WM_GETTEXTLENGTH, 0, 0);
+				lpszFileText = (LPSTR)GlobalAlloc(GPTR, dwTextLength + 1);
+				SendMessage(hEdit, WM_GETTEXT, dwTextLength + 1, (LPARAM)lpszFileText);
 				break;
 			}
 
@@ -187,37 +269,54 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			if (GetSaveFileName(&ofn))
 			{
 				HWND hEdit = GetDlgItem(hwnd, IDC_EDIT);
-				SaveTextFileFromEdit(hEdit, szFileName, lpszFileText);
+				SaveTextFileFromEdit(hEdit, szFileName);
 			}
+			CHAR szTitle[FILENAME_MAX]{};
+			sprintf(szTitle, "%s - %s", g_sz_WINDOW_CLASS, strrchr(szFileName, '\\') + 1);
+			SendMessage(hwnd, WM_SETTEXT, 0, (LPARAM)szTitle);
+			HWND hStatusBar = GetDlgItem(hwnd, ID_STATUS);
+			SendMessage(hStatusBar, WM_SETTEXT, 0, (LPARAM)szFileName);
 		}
 		break;
+		case ID_FILE_EXIT: SendMessage(hwnd, WM_CLOSE, 0, 0);  break;
+			///////////////////////////////////////////////
+		case ID_FORMAT_FONT:SelectFont(hwnd);	break;
 		}
 	}
 	break;
 	case WM_DESTROY:	PostQuitMessage(0); break;
 	case WM_CLOSE:
 	{
-		if(FileWasChanger(GetDlgItem(hwnd, IDC_EDIT), lpszText, lpszFileText))
+		BOOL close = FALSE;
+		HWND hEdit = GetDlgItem(hwnd, IDC_EDIT);
+		DWORD dwTextLength = SendMessage(hEdit, WM_GETTEXTLENGTH, 0, 0);
+		LPSTR lpszText = (LPSTR)GlobalAlloc(GPTR, dwTextLength + 1);
+
+		if (lpszText != NULL)
 		{
-			switch (INT answer = MessageBox(hwnd, "Сохранить изменения в файле?", "Question", MB_YESNOCANCEL | MB_ICONQUESTION))
+			SendMessage(hEdit, WM_GETTEXT, dwTextLength + 1, (LPARAM)lpszText);
+			if (lpszFileText && strcmp(lpszFileText, lpszText) || lpszFileText == NULL && lpszText[0])
 			{
-			case IDYES: SendMessage(hwnd, WM_COMMAND, ID_FILE_SAVE, 0); 
-			case IDNO: DestroyWindow(hwnd); 
-			case IDCANCEL: break;
+				switch (INT answer = MessageBox(hwnd, "Сохранить изменения в файле?", "Question", MB_YESNOCANCEL | MB_ICONQUESTION))
+				{
+				case IDYES: SendMessage(hwnd, WM_COMMAND, ID_FILE_SAVE, 0);
+				case IDNO: close = TRUE;
+				case IDCANCEL: break;
+				}
+			}
+			else
+			{
+				close = TRUE;
 			}
 		}
-		else
-		{
-			DestroyWindow(hwnd);
-		}
-		
+		if (close)DestroyWindow(hwnd);
 	}
 	break;
 	default:			return DefWindowProc(hwnd, uMsg, wParam, lParam);
 	}
 	return NULL;
 }
-BOOL LoadTextFileToEdit(HWND hEdit, LPSTR lpszFileName, LPSTR lpszText)
+BOOL LoadTextFileToEdit(HWND hEdit, LPSTR lpszFileName)
 {
 	BOOL bSuccess = FALSE;
 	HANDLE hFile = CreateFile(lpszFileName, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, 0, NULL);
@@ -226,7 +325,7 @@ BOOL LoadTextFileToEdit(HWND hEdit, LPSTR lpszFileName, LPSTR lpszText)
 		DWORD dwFileSize = GetFileSize(hFile, NULL);
 		if (dwFileSize != UINT_MAX)
 		{
-			lpszText = (LPSTR)GlobalAlloc(GPTR, dwFileSize + 1);
+			LPSTR lpszText = (LPSTR)GlobalAlloc(GPTR, dwFileSize + 1);
 			if (lpszText)
 			{
 
@@ -243,7 +342,7 @@ BOOL LoadTextFileToEdit(HWND hEdit, LPSTR lpszFileName, LPSTR lpszText)
 	}
 	return bSuccess;
 }
-BOOL SaveTextFileFromEdit(HWND hEdit, LPSTR lpszFileName, LPSTR lpszFileText )
+BOOL SaveTextFileFromEdit(HWND hEdit, LPSTR lpszFileName)
 {
 	BOOL bSuccess = FALSE;
 	HANDLE hFile = CreateFile(lpszFileName, GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
@@ -252,7 +351,7 @@ BOOL SaveTextFileFromEdit(HWND hEdit, LPSTR lpszFileName, LPSTR lpszFileText )
 		DWORD dwTextLength = GetWindowTextLength(hEdit);
 		if (dwTextLength > 0)
 		{
-			lpszFileText = (LPSTR)GlobalAlloc(GPTR, dwTextLength + 1);
+			LPSTR lpszFileText = (LPSTR)GlobalAlloc(GPTR, dwTextLength + 1);
 			if (lpszFileText != NULL)
 			{
 				if (GetWindowText(hEdit, lpszFileText, dwTextLength + 1))
@@ -290,4 +389,34 @@ BOOL FileWasChanger(HWND hEdit, LPSTR lpszText, LPSTR lpszFileText)
 		}
 	}
 	return fileChange;
+}
+
+VOID SelectFont(HWND hwnd)
+{
+	CHOOSEFONT cf;
+	LOGFONT lf;
+	HWND hEdit = GetDlgItem(hwnd, IDC_EDIT);
+
+	ZeroMemory(&cf, sizeof(cf));
+	ZeroMemory(&lf, sizeof(lf));
+
+	GetObject(g_hFont, sizeof(LOGFONT), &lf);
+
+	cf.lStructSize = sizeof(cf);
+	cf.hwndOwner = hwnd;
+
+	cf.Flags = CF_EFFECTS | CF_INITTOLOGFONTSTRUCT | CF_SCREENFONTS;
+	cf.hInstance = GetModuleHandle(NULL);
+	cf.lpLogFont = &lf;
+	cf.rgbColors = g_RGB_Text;
+
+	if (ChooseFont(&cf))
+	{
+		HFONT hf = CreateFontIndirect(&lf);
+		if (hf)g_hFont = hf;
+		else MessageBox(hwnd, "Font creation failde", "error", MB_OK | MB_ICONERROR);
+
+	}
+	SendMessage(hEdit, WM_SETFONT, (WPARAM)g_hFont, TRUE);
+	SetFocus(hEdit);
 }
--- TextEditor/resource.h
+++ TextEditor/resource.h
@@ -2,8 +2,9 @@
 // Microsoft Visual C++ generated include file.
 // Used by Resource.rc
 //
-#define IDC_EDIT						100
+#define IDC_EDIT                        100
 #define IDR_MENU1                       101
+#define ID_STATUS                       102
 #define ID_FILE_NEW                     40001
 #define ID_FILE_OPEN                    40002
 #define ID_FILE_SAVE                    40003
@@ -14,15 +15,14 @@
 #define ID_EDIT_PAST                    40008
 #define ID_EDIT_FIND                    40009
 #define ID_HELP_ADOUT                   40010
-#define IDSTATUS		                40011
-
+#define ID_FORMAT_FONT                  40011
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        102
-#define _APS_NEXT_COMMAND_VALUE         40011
+#define _APS_NEXT_COMMAND_VALUE         40012
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
